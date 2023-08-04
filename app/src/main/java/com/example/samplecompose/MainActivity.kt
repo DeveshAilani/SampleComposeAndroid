68a1e6dd0d09636381f76e1022cadf7e3dcbb460
Devesh Ailani <devesh.ailani@lenskart.in>
2023-08-04 18:33:29 +0530

    - Padding - Border - Offset (margin) - Clickable

--- app/src/main/java/com/example/samplecompose/MainActivity.kt
+++ app/src/main/java/com/example/samplecompose/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.samplecompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,6 +28,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.samplecompose.ui.theme.SampleComposeTheme
@@ -25,56 +37,36 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SampleComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Row {
-                        Column(
-                            modifier = Modifier
-                                .fillMaxSize(0.5f)
-                                .background(Color.Blue),
-                            verticalArrangement = Arrangement.Center
-                        ) {
-                            Greeting("Waheguru Ji 1")
-                            Greeting("Waheguru Ji 2")
-                        }
+            Column(
+                modifier = Modifier
+                    // This modifiers will applies sequentially
+                    .background(Color.LightGray)
+                    .fillMaxWidth()
+                    // .width(300.dp)
+                    // .requiredWidth(600.dp)
+                    .fillMaxHeight(0.5f)
+                    .border(5.dp, Color.Yellow, shape = RoundedCornerShape(15.dp))
+                    .padding(16.dp, 32.dp)
+                    .border(5.dp, Color.Green)
+                    .padding(12.dp)
+            ) {
+                Text(
+                    text = "Hello",
+                    modifier = Modifier
+                        .background(Color.DarkGray)
+                        .offset(x = 15.dp, y = 5.dp)
+                )
 
-                        Column(
-                            modifier = Modifier
-                                //.fillMaxSize() // similar to match parent
-                                .width(200.dp)
-                                .height(300.dp)
+                Spacer(modifier = Modifier.height(50.dp))
 
-                                .background(Color.Green),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.SpaceEvenly
-                        ) {
-                            Greeting("Waheguru Ji 3")
-                            Greeting("Waheguru Ji 4")
-                            Greeting("Waheguru Ji 5")
-                        }
-                    }
-                }
+                Text(text = "World 1", modifier = Modifier
+                    .background(Color.White)
+                    .clickable {
+                        Toast
+                            .makeText(this@MainActivity, "Something clicked", Toast.LENGTH_LONG)
+                            .show()
+                    })
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "-- $name --",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SampleComposeTheme {
-        Greeting("Waheguru Ji")
     }
 }
