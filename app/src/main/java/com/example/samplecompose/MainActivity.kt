39f5e14b41a05b54ae525e009390e3e416519a2d
Devesh Ailani <devesh.ailani@lenskart.in>
2023-08-07 19:00:07 +0530

    - Image card

--- app/src/main/java/com/example/samplecompose/MainActivity.kt
+++ app/src/main/java/com/example/samplecompose/MainActivity.kt
@@ -4,10 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,51 +23,91 @@ import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.samplecompose.ui.theme.SampleComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            val painter = painterResource(id = R.drawable.kermit_snow)
+            val contentDescription = "Kermit is playing in the snow"
+            val title = "Kermit is playing in the snow"
+
+            Box(
                 modifier = Modifier
-                    // This modifiers will applies sequentially
-                    .background(Color.LightGray)
-                    .fillMaxWidth()
-                    // .width(300.dp)
-                    // .requiredWidth(600.dp)
-                    .fillMaxHeight(0.5f)
-                    .border(5.dp, Color.Yellow, shape = RoundedCornerShape(15.dp))
-                    .padding(16.dp, 32.dp)
-                    .border(5.dp, Color.Green)
-                    .padding(12.dp)
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
+            ) {
+                ImageCard(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    title = title
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(Color.Transparent, Color.Black),
+                        startY = 300f
+                    )
+                )) {
+
+            }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
             ) {
                 Text(
-                    text = "Hello",
-                    modifier = Modifier
-                        .background(Color.DarkGray)
-                        .offset(x = 15.dp, y = 5.dp)
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 16.sp)
                 )
-
-                Spacer(modifier = Modifier.height(50.dp))
-
-                Text(text = "World 1", modifier = Modifier
-                    .background(Color.White)
-                    .clickable {
-                        Toast
-                            .makeText(this@MainActivity, "Something clicked", Toast.LENGTH_LONG)
-                            .show()
-                    })
             }
         }
     }
